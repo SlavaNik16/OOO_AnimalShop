71c6a30d4c5382c665b09bde467952599452a260
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-02-16 21:30:34 +0300

    Тупой гитхаб

--- OOO_AnimalShop.Context/AnimalShopContext.cs
+++ OOO_AnimalShop.Context/AnimalShopContext.cs
@@ -12,6 +12,7 @@ namespace OOO_AnimalShop.Context
     {
         public DbSet<Order> Orders { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductTypes> ProductTypeses { get; set; }
         public DbSet<PunktVidachi> PunktVidachis { get; set; }
